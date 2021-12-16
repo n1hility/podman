5852cee4c517f303ed5122ea28cd9b1dad6e04c2
Jason T. Greene <jason.greene@redhat.com>
2021-12-15 22:42:46 -0600

    Preload the kernel if we can
    [NO NEW TESTS NEEDED]
    
    Signed-off-by: Jason T. Greene <jason.greene@redhat.com>

---
+++ pkg/machine/wsl/arch_windows_amd64.go
@@ -0,0 +1,7 @@
+package wsl
+
+import "net/url"
+
+var (
+	kernelArchURL, _ = url.Parse("https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi")
+)
---
+++ pkg/machine/wsl/arch_windows_arm64.go
@@ -0,0 +1,5 @@
+package wsl
+
+var (
+	kernelArchURL, _ = url.Parse("https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_arm64.msi")
+)
