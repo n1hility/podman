5224e5101ba74c473bf416848c530e25d096575e
Jason T. Greene <jason.greene@redhat.com>
2022-04-15 23:55:14 -0500

    wip

--- pkg/specgen/winpath.go
+++ pkg/specgen/winpath.go
@@ -14,7 +14,7 @@ func isHostWinPath(path string) bool {
 }
 
 func hasWinDriveScheme(path string, start int) bool {
-	if len(path) < start + 1 || path[start + 1] != ':' {
+	if len(path) < start+1 || path[start+1] != ':' {
 		return false
 	}
 
@@ -22,25 +22,29 @@ func hasWinDriveScheme(path string, start int) bool {
 	return drive < unicode.MaxASCII && unicode.IsLetter(drive)
 }
 
-// Converts a Windows path to a WSL guest path if local env is a WSL linux guest or this is a Windows client. 
+// Converts a Windows path to a WSL guest path if local env is a WSL linux guest or this is a Windows client.
 func ConvertWinMountPath(path string) (string, error) {
 	if !shouldResolveWinPaths() {
+		fmt.Println("Nope!")
 		return path, nil
 	}
+
+	fmt.Println("Yep!")
 
 	if strings.HasPrefix(path, "/") {
 		// Handle /[driveletter]/windows/path form (e.g. c:\Users\bar == /c/Users/bar)
 		if len(path) > 2 && path[2] == '/' && shouldResolveUnixWinVariant(path) {
-			drive := unicode.ToLower(rune(path[1])) 
+			drive := unicode.ToLower(rune(path[1]))
 			if unicode.IsLetter(drive) && drive <= unicode.MaxASCII {
-				winPath := fmt.Sprintf("%c:%s", drive,  strings.ReplaceAll(path[2:], "/", `\`))
+				winPath := fmt.Sprintf("%c:%s", drive, strings.ReplaceAll(path[2:], "/", `\`))
 				if _, err := os.Stat(winPath); err == nil {
+					fmt.Println("Return")
 					return fmt.Sprintf("/mnt/%c/%s", drive, path[3:]), nil
 				}
 			}
-		} 
+		}
 
-		// unix path - pass through 
+		// unix path - pass through
 		return path, nil
 	}
 
@@ -55,7 +59,7 @@ func ConvertWinMountPath(path string) (string, error) {
 	if strings.HasPrefix(path, `\\.\`) {
 		path = "/mnt/wsl/" + path[4:]
 	} else if path[1] == ':' {
-		path = "/mnt/" + strings.ToLower(path[0:1]) + path[2:]		
+		path = "/mnt/" + strings.ToLower(path[0:1]) + path[2:]
 	} else {
 		return path, errors.New("unsupported UNC path")
 	}
